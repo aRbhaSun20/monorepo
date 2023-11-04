643a870a8b140e9837e674fab1862aee4d60e0e0
aRbhaSun20 <remo78295279@gmail.com>
2023-11-04 20:24:30 +0530

    inventory and shared components added

--- apps/inventory/src/App.tsx
+++ apps/inventory/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { SharedComponents } from '@monorepo/shared-components';
 
 function App() {
   return (
-    <div>Inventory</div>
-  )
+    <div>
+      Inventory
+      <SharedComponents />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- apps/react-store/src/App.tsx
+++ apps/react-store/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { SharedComponents } from '@monorepo/shared-components';
 
 function App() {
   return (
-    <div>Host 1</div>
-  )
+    <div>
+      Host 1
+      <SharedComponents />
+    </div>
+  );
 }
 
-export default App
+export default App;
